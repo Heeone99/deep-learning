76d3f0bd8b1e49f46e6f877931fe2dca6aa240cf
Heeone99 <heeone999@gmail.com>
2025-01-06 15:45:33 +0900

    하이퍼볼릭 탄젠트 미분

---
+++ ch3.실습/step35.py
@@ -0,0 +1,29 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero import Variable
+from dezero.utils import plot_dot_graph
+import dezero.functions as F
+
+
+"""
+하이퍼볼릭 탄젠트 함수 고차 미분
+"""
+
+x = Variable(np.array(1.0))
+y = F.tanh(x)
+x.name = 'x'
+y.name = 'y'
+y.backward(create_graph=True)
+
+iters = 1
+
+for i in range(iters):
+    gx = x.grad
+    x.cleargrad()
+    gx.backward(create_graph=True)
+
+gx = x.grad
+gx.name = 'gx' + str(iters + 1)
+plot_dot_graph(gx, verbose=False, to_file='tanh.png')
