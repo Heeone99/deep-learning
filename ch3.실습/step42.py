4d394e66fa79f634a4da00719b1509d4b4cc9622
Heeone99 <heeone999@gmail.com>
2025-01-07 16:47:28 +0900

    선형 회귀

--- ch3.실습/step42.py
+++ ch3.실습/step42.py
@@ -35,6 +35,24 @@ def predict(x):
 
 # MSE(평균 제곱 오차)를 손실 함수로 사용
 # 이를 줄이는 방향으로 학습
+
+# class MeanSquaredError(Function):
+#     def forward(self, x0, x1):
+#         diff = x0 - x1
+#         y = (diff ** 2).sum() / len(diff)
+#         return y
+
+#     def backward(self, gy):
+#         x0, x1 = self.inputs
+#         diff = x0 - x1
+#         gx0 = gy * diff * (2. / len(diff))
+#         gx1 = -gx0
+#         return gx0, gx1
+
+
+# def mean_squared_error(x0, x1):
+#     return MeanSquaredError()(x0, x1)
+
 def mean_squared_error(x0, x1):
     diff = x0 - x1
     return F.sum(diff ** 2) / len(diff)
