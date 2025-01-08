ca067e72e431db528e2e4fbe9dbef6d5e3f28ab1
Heeone99 <heeone999@gmail.com>
2025-01-08 14:18:58 +0900

    Optimizer 구현

---
+++ ch3.실습/step46.py
@@ -0,0 +1,73 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero import optimizers
+import dezero.functions as F
+from dezero.models import MLP
+
+
+"""
+Optimizer 구현
+"""
+
+
+# Optimizer
+class Optimizer:
+    def __init__(self):
+        self.target = None
+        self.hooks = []
+
+    def setup(self, target):
+        self.target = target
+        return self
+
+    def update(self):
+        params = [p for p in self.target.params() if p.grad is not None]
+
+        for f in self.hooks:
+            f(params)
+
+        for param in params:
+            self.update_one(param)
+
+    def update_one(self, param):
+        raise NotImplementedError()
+
+    def add_hook(self, f):
+        self.hooks.append(f)
+
+
+# SGD
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        super().__init__()
+        self.lr = lr
+
+    # 매개변수 갱신
+    def update_one(self, param):
+        param.data -= self.lr * param.grad.data
+
+
+# 문제 해결
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+lr = 0.2
+max_iter = 10000
+hidden_size = 10
+
+model = MLP((hidden_size, 1))
+optimizer = optimizers.SGD(lr).setup(model)
+
+for i in range(max_iter):
+    y_pred = model(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    model.cleargrads()
+    loss.backward()
+
+    optimizer.update()
+    if i % 1000 == 0:
+        print(loss)
