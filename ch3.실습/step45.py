7e80256361e24cdcc9d170e53866eb9423038c30
Heeone99 <heeone999@gmail.com>
2025-01-08 14:04:32 +0900

    MLP 구현

---
+++ ch3.실습/step45.py
@@ -0,0 +1,59 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero import Model
+import dezero.layers as L
+import dezero.functions as F
+
+
+"""
+Model 클래스를 활용한 MLP 구현
+"""
+
+
+# Model 클래스
+# Layer 클래스 상속
+
+# class Model(Layer):
+#     def plot(self, *inputs, to_file='model.png'):
+#         y = self.forward(*inputs)
+#         return utils.plot_dot_graph(y, verbose=True, to_file=to_file)
+
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+# 하이퍼 파라미터 설정
+lr = 0.2
+max_iter = 10000
+hidden_size = 10
+
+# 모델 정의
+# Layer들을 쉽게 다루기 위한 클래스
+class TwoLayerNet(Model):
+    def __init__(self, hidden_size, out_size):
+        super().__init__()
+        self.l1 = L.Linear(hidden_size)
+        self.l2 = L.Linear(out_size)
+
+    def forward(self, x):
+        y = F.sigmoid(self.l1(x))
+        y = self.l2(y)
+        return y
+
+model = TwoLayerNet(hidden_size, 1)
+
+# 학습
+for i in range(max_iter):
+    y_pred = model(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    model.cleargrads()
+    loss.backward()
+
+    for p in model.params():
+        p.data -= lr * p.grad.data
+    if i % 1000 == 0:
+        print(loss)
